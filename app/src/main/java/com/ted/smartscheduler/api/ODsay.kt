064284f34dd7b482eec5ce0126dba6fc3e706561
taweon-ted <taewon.ted@navercorp.com>
2020-01-15 23:25:01 +0900

    [ODsay] 라이브러리 추가
    - Retrofit, Gson, RxKotlin 추가
    - ODsay 인터페이스 추가

---
+++ app/src/main/java/com/ted/smartscheduler/api/ODsay.kt
@@ -0,0 +1,17 @@
+package com.ted.smartscheduler.api
+
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ODsay {
+
+    // 대중교통 길찾기
+    @GET("ODsayService.requestSearchPubTransPath")
+    fun findPath(
+        @Query("SX") startX: Float,
+        @Query("SY") startY: Float,
+        @Query("EX") endX: Float,
+        @Query("EY") endY: Float
+    ): Single<Unit>
+}
